25cc1d58f3168a4a4606e0a5977fa0466e0a26f0
gus-codes <gusmnk.dev@gmail.com>
2022-11-10 09:18:34 -0300

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+$('.b1').click(function(){
+    $('h1').css('color', 'purple')
+});
+$('.b2').click(function(){
+    $('h1').css('color', 'red')
+});
+$('.b3').click(function(){
+    $('h1').css('color', 'green')
+});
+$('.b4').click(function(){
+    $('h1').css('color', 'yellow')
+});
+$('.b5').click(function(){
+    $('h1').css('color', 'orange')
+});
